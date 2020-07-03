03d5b7d48fc948921bcb7ea6f1d206d9956f11af
jderesz <janusz@pdot.eu>
2020-07-03 18:24:17 +0200

    Employee class

---
+++ src/com/company/Employee.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private Double dailyPayment;
+
+    public boolean frontendSkill = false;
+    public boolean backendSkill = false;
+    public boolean databaseSkill = false;
+    public boolean mobileSkill = false;
+    public boolean wordpressSkill = false;
+    public boolean prestashopSkill = false;
+
+    public Employee(String name, Double dailyPayment) {
+        this.name = name;
+        this.dailyPayment = dailyPayment;
+    }
+
+    @Override
+    public String toString() {
+        List<String> skills = new ArrayList<String>();
+        if (this.frontendSkill) skills.add("frontend");
+        if (this.backendSkill) skills.add("backend");
+        if (this.databaseSkill) skills.add("database");
+        if (this.mobileSkill) skills.add("mobile");
+        if (this.wordpressSkill) skills.add("wordpress");
+        if (this.prestashopSkill) skills.add("prestashop");
+        return "Nazwa: " + this.name +
+                "\nDzienna wypłata: " + this.dailyPayment +
+                "\nUmiejętności: " + String.join(", ", skills);
+    }
+}
